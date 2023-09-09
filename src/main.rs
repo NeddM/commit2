fe464b3332dc1d20a77acf41c3ea43442daa4963
Nedd Chairi Muñoz <neddchairiweb@gmail.com>
2023-09-09 23:28:16 +0200

    emoji jfklsdjklfdsjklfsd

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,27 @@ fn main() {
     println!("Comment of the commit: ");
     let stdin = std::io::stdin();
     let mut comment = String::new();
+    // process::Command::new("pwd").spawn().expect("Could not pwd");
 
     stdin.read_line(&mut comment).unwrap();
-    // println!("{}", comment);
-    commit_process(comment)
+    commit_process(comment);
+    push_process();
 }
 
 fn commit_process(comment: String) {
     println!("{}", comment);
-    process::Command::new("git commit -m ").args(["emoji", &comment]);
+
+    let processed_comment = ["emoji", &comment].join(" ");
+
+    process::Command::new("git")
+        .args(["commit", "-m", &processed_comment])
+        .spawn()
+        .expect("Could not commit");
+}
+
+fn push_process() {
+    process::Command::new("git")
+        .args(["commit", "push"])
+        .spawn()
+        .expect("Could not push");
 }
